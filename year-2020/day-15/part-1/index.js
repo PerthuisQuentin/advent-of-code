9965c8517d206c301192b2def9e9881be9b26694
PerthuisQuentin <perthuis.quentin@gmail.com>
2020-12-15 14:53:54 +0100

    :sparkles: [2020] Add day 15

---
+++ year-2020/day-15/part-1/index.js
@@ -0,0 +1,43 @@
+class MemoryGame {
+	constructor(input) {
+		this.memory = {}
+		this.turn = 1
+		this.lastSpoken = null
+		this.loadInput(input)
+	}
+
+	say(n) {
+		if (this.lastSpoken !== null) {
+			this.memory[this.lastSpoken] = this.turn - 1
+		}
+		this.lastSpoken = n
+		this.turn++
+	}
+
+	loadInput(input) {
+		input[0]
+			.split(',')
+			.map(Number)
+			.forEach(n => this.say(n))
+	}
+
+	doTurn() {
+		if (this.memory[this.lastSpoken] === undefined) {
+			this.say(0)
+		} else {
+			this.say((this.turn - 1) - this.memory[this.lastSpoken])
+		}
+	}
+
+	doTurnUntil(stopTurn) {
+		while (this.turn < stopTurn) this.doTurn()
+	}
+}
+
+const contestResponse = input => {
+	const memoryGame = new MemoryGame(input)
+	memoryGame.doTurnUntil(2021)
+	return memoryGame.lastSpoken
+}
+
+module.exports = contestResponse
