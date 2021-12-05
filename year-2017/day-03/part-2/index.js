62116752b3ad996792063ffb2676f413253f601d
PerthuisQuentin <perthuis.quentin@gmail.com>
2021-12-05 19:29:48 +0100

    :sparkles: [2017] Add day 3 part 2

--- year-2017/day-03/part-2/index.js
+++ year-2017/day-03/part-2/index.js
@@ -1,30 +1,48 @@
-const contestResponse = input => {
-	const max = Number(input[0])
-	let value = 1
-	let pos = { x: 0, y: 0 }
-	let step = 1
-	let dir
+const Grid = require('../../../utils/grid')
 
-	for (let i = 1; i <= max; i++) {
+function* positions() {
+	let pos = { x: 0, y: 0 }
+	let i = 1
+	let dir
+	let step = 1
+
+	while(true) {
 		dir = step % 2 === 1 ? 1 : -1
 
 		for (let y = 0; y < step; y++) {
-			if (value === max) break
-			value++
 			pos.x += dir
+			yield pos
 		}
 		for (let y = 0; y < step; y++) {
-			if (value === max) break
-			value++
-			pos.y += dir
+			pos.y -= dir
+			yield pos
 		}
 
 		step++
 	}
+}
 
-	const distance = Math.abs(pos.x) + Math.abs(pos.y)
-	
-	return distance.toString()
+const contestResponse = input => {
+	const max = Number(input[0])
+	const grid = new Grid(0)
+
+	grid.setCell({ x: 0, y: 0 }, 1)
+
+	const iterator = positions(0);
+
+	let value = 1
+
+	while (value < max) {
+		const position = iterator.next().value
+		const neighbors = grid.getNeighborsWithDiagonals(position)
+		const sum = neighbors.reduce((a, b) => a + b)
+		grid.setCell(position, sum)
+		value = sum
+	}
+
+	console.log(grid.toParsedString(x => `\t${x}`))
+
+	return value
 }
 
 module.exports = contestResponse
