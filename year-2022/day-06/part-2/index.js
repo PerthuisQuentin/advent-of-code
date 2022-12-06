784e2c97933ffddb7db9c7476994e6ae031c5dde
PerthuisQuentin <perthuis.quentin@gmail.com>
2022-12-06 13:13:04 +0100

    :sparkles: [2022] Add day 6

---
+++ year-2022/day-06/part-2/index.js
@@ -0,0 +1,14 @@
+const DISTINCT_CHARACTERS = 14
+
+const areAllDifferent = characters => characters
+	.every((charA, indexA) => !characters.some((charB, indexB) => indexA !== indexB && charA === charB))
+
+const contestResponse = input => {
+	return input[0].split('').findIndex((_, index, list) => {
+		if (index < (DISTINCT_CHARACTERS - 1)) return false
+		const lastChars = list.slice(index - (DISTINCT_CHARACTERS - 1), index + 1)
+		return areAllDifferent(lastChars)
+	}) + 1
+}
+
+module.exports = contestResponse
