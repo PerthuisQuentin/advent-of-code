98db68425bf4b0d20ed778ab8b2b29f522a43d6b
Quentin Perthuis <perthuis.quentin@gmail.com>
2024-12-11 07:53:26 +0100

    :sparkles: Improve parseInputToGrid to accept a transform callback

--- src/utils/input.ts
+++ src/utils/input.ts
@@ -1,6 +1,9 @@
 import { FixedGrid2D } from './fixed-grid-2d'
 
-export const parseInputToGrid = (input: string[]): FixedGrid2D<string> => {
-  const arrayGrid = input.map((row) => row.split(''))
-  return new FixedGrid2D<string>(arrayGrid)
+export const parseInputToGrid = <T = string>(
+  input: string[],
+  transform: (cell: string) => T = (cell) => cell as T,
+): FixedGrid2D<T> => {
+  const arrayGrid = input.map((row) => row.split('').map(transform))
+  return new FixedGrid2D<T>(arrayGrid)
 }
