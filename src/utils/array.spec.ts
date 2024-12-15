b5f4bccc1ec84fa72b8d56849424c5a764f2389d
Quentin Perthuis <perthuis.quentin@gmail.com>
2024-12-15 20:07:04 +0100

    :sparkles: Add multiply and split to array utils

--- src/utils/array.spec.ts
+++ src/utils/array.spec.ts
@@ -1,7 +1,7 @@
-import { count, max, min, sum } from './array'
+import { count, max, min, multiply, split, sum } from './array'
 
 describe('Array', () => {
-  describe('Sum', () => {
+  describe('sum', () => {
     it('Should return the sum of the list', () => {
       expect(sum([1, 2, 3, 4, 5])).toBe(15)
     })
@@ -15,7 +15,21 @@ describe('Array', () => {
     })
   })
 
-  describe('Min', () => {
+  describe('multiply', () => {
+    it('Should return the product of the list', () => {
+      expect(multiply([1, 2, 3, 4, 5])).toBe(120)
+    })
+
+    it('Should return the only value in a list of 1 element', () => {
+      expect(multiply([5])).toBe(5)
+    })
+
+    it('Should throw an error when the list is empty.', () => {
+      expect(() => multiply([])).toThrow('Empty list')
+    })
+  })
+
+  describe('min', () => {
     it('Should return the min of the list', () => {
       expect(min([4, 8, 3, 9, 4])).toBe(3)
     })
@@ -29,7 +43,7 @@ describe('Array', () => {
     })
   })
 
-  describe('Max', () => {
+  describe('max', () => {
     it('Should return the max of the list', () => {
       expect(max([4, 8, 3, 9, 4])).toBe(9)
     })
@@ -43,7 +57,7 @@ describe('Array', () => {
     })
   })
 
-  describe('Count', () => {
+  describe('count', () => {
     it('Should return the count of elements greater than 3', () => {
       expect(count([1, 2, 3, 4, 5], (x) => x > 3)).toBe(2)
     })
@@ -58,6 +72,39 @@ describe('Array', () => {
 
     it('Should return 0 for an empty list', () => {
       expect(count([], (x) => x > 0)).toBe(0)
+    })
+  })
+
+  describe('split', () => {
+    it('Should split array in multiple array while excluding the separator', () => {
+      expect(split([1, 2, 3, 4, 5, 6, 7, 8, 9], 5)).toEqual([
+        [1, 2, 3, 4],
+        [6, 7, 8, 9],
+      ])
+    })
+
+    it('Should return the original array if the separator is not found', () => {
+      expect(split([1, 2, 3, 4, 5, 6, 7, 8, 9], 10)).toEqual([[1, 2, 3, 4, 5, 6, 7, 8, 9]])
+    })
+
+    it('Should return an empty array if the input array is empty', () => {
+      expect(split([], 10)).toEqual([[]])
+    })
+
+    it('Should return an array of empty array if the separator is the first element', () => {
+      expect(split([1, 2, 3, 4, 5, 6, 7, 8, 9], 1)).toEqual([[], [2, 3, 4, 5, 6, 7, 8, 9]])
+    })
+
+    it('Should return an array of empty array if the separator is the last element', () => {
+      expect(split([1, 2, 3, 4, 5, 6, 7, 8, 9], 9)).toEqual([[1, 2, 3, 4, 5, 6, 7, 8], []])
+    })
+
+    it('Should return an array of empty array if the separator is the only element', () => {
+      expect(split([1], 1)).toEqual([[], []])
+    })
+
+    it('Should split srting array', () => {
+      expect(split(['hello', '', 'world'], '')).toEqual([['hello'], ['world']])
     })
   })
 })
--- src/utils/array.ts
+++ src/utils/array.ts
@@ -4,6 +4,12 @@ export const sum = (list: number[]): number => {
   return list.reduce((a, b) => a + b, 0)
 }
 
+export const multiply = (list: number[]): number => {
+  if (!list.length) throw new Error('Empty list')
+  if (list.length === 1) return list[0]!
+  return list.reduce((a, b) => a * b, 1)
+}
+
 export const min = (list: number[]): number => {
   if (!list.length) throw new Error('Empty list')
   if (list.length === 1) return list[0]!
@@ -18,4 +24,18 @@ export const max = (list: number[]): number => {
 
 export const count = <T>(list: T[], predicate: (item: T) => boolean): number => {
   return list.reduce((acc, item) => (predicate(item) ? acc + 1 : acc), 0)
+}
+
+export const split = <T>(array: T[], separator: T): T[][] => {
+  return array.reduce(
+    (result, current) => {
+      if (current === separator) {
+        result.push([])
+      } else {
+        result[result.length - 1]!.push(current)
+      }
+      return result
+    },
+    [[]] as T[][],
+  )
 }
