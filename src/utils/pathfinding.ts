7cf484639c041b49c17306c6b42145976613810e
Quentin Perthuis <perthuis.quentin@gmail.com>
2024-12-22 16:38:52 +0100

    :sparkles: Add a getAccessibleCells method in pathfinding utils

--- src/utils/pathfinding.ts
+++ src/utils/pathfinding.ts
@@ -1,7 +1,7 @@
 import { Cell2D, FixedGrid2D } from './fixed-grid-2d'
 import { Point2D } from './point-2d'
 
-export const breadthFirstSearch = <T>(
+export const exploreByBreadthFirstSearch = <T>(
   grid: FixedGrid2D<T>,
   isWalkable: (cell: Cell2D<T>) => boolean,
   start: Point2D,
@@ -9,6 +9,7 @@ export const breadthFirstSearch = <T>(
 ): Map<string, Cell2D<T>> => {
   const startCell = grid.getCell(start)
   if (!startCell) throw new Error('Start cell not found')
+
   const queue: Cell2D<T>[] = [startCell]
   const parentByHash = new Map<string, Cell2D<T>>()
   const distanceByHash = new Map<string, number>()
@@ -53,4 +54,41 @@ export const backtrace = (
     current = next
   }
   return path.reverse()
+}
+
+export const getAccessibleCells = <T>(
+  grid: FixedGrid2D<T>,
+  isWalkable: (cell: Cell2D<T>) => boolean,
+  start: Point2D,
+  maxDistance: number,
+): Map<string, number> => {
+  const startCell = grid.getCell(start)
+  if (!startCell) throw new Error('Start cell not found')
+
+  let queue: Cell2D<T>[] = [startCell]
+  let nextQueue: Cell2D<T>[] = []
+  const accessibleCells = new Map<string, number>()
+  accessibleCells.set(start.hash, 0)
+
+  for (let step = 0; step < maxDistance; step++) {
+    for (const current of queue) {
+      for (const neighbor of grid.getNeighbors(current)) {
+        if (!isWalkable(neighbor)) {
+          continue
+        }
+
+        if (accessibleCells.has(neighbor.hash)) {
+          continue
+        }
+
+        accessibleCells.set(neighbor.hash, step + 1)
+        nextQueue.push(neighbor)
+      }
+    }
+
+    queue = nextQueue
+    nextQueue = []
+  }
+
+  return accessibleCells
 }
